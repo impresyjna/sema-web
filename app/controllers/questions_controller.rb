b2ea21664569d8319b6e94b34ce6ea73b512b101
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-05-31 15:46:21 +0200

    CRUD for question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,83 +1,59 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: [:show, :edit, :update, :destroy]
-
-  # GET /questions
-  # GET /questions.json
   def index
     @questions = Question.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @questions }
-    end
   end
 
-  # GET /questions/1
-  # GET /questions/1.json
-  def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @question }
-    end
-  end
-
-  # GET /questions/new
   def new
     @question = Question.new
+    @categories = Category.all.pluck(:name, :id)
   end
 
-  # GET /questions/1/edit
   def edit
+    @question = Question.find(params[:id])
+    @categories = Category.all.pluck(:name, :id)
   end
 
-  # POST /questions
-  # POST /questions.json
   def create
     @question = Question.new(question_params)
-
-    respond_to do |format|
-      if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
-        format.json { render json: @question, status: :created }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
+    if @question.save
+      flash[:success] = "Dodano pytanie"
+      redirect_to questions_path
+    else
+      flash[:warning] = "Nie udało się dodać pytania"
+      render 'new'
     end
   end
 
-  # PATCH/PUT /questions/1
-  # PATCH/PUT /questions/1.json
   def update
-    respond_to do |format|
-      if @question.update(question_params)
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
+    @question = Question.find(params[:id])
+    if @question.update_attributes(question_params)
+      flash[:success] = "Dane zaktualizowane"
+      redirect_to questions_path
+    else
+      render 'edit'
     end
   end
 
-  # DELETE /questions/1
-  # DELETE /questions/1.json
   def destroy
-    @question.destroy
-    respond_to do |format|
-      format.html { redirect_to questions_url }
-      format.json { head :no_content }
+    @question = Question.find(params[:id])
+    if @question.confirmed
+      @question.confirmed = false
+    else
+      @question.confirmed = true
+    end
+    if @question.save
+      flash[:success] = "Pomyślnie zmieniono"
+      redirect_to questions_path
+    else
+      flash[:warning] = "Nie udało się zmienić"
+      redirect_to questions_path
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_question
-      @question = Question.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def question_params
-      params[:question]
-    end
+  def question_params
+    params.require(:question).permit(:content, :category_id, :confirmed)
+  end
+
 end
