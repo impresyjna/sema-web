e1e42d0666c2e8a8f34449e00be06a8cd294ceae
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-05-31 16:03:30 +0200

    Dashboard data in index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < FrontController
   def index
+    @questions_count = Question.all.count
+    @cards_count = Card.all.count
+    @categories_count = Category.all.count
+    @unconfirmed_questions_count = Question.where(confirmed: false).count
+    @users_count = User.where(admin: false).count
+    @rooms_count = Room.all.count
   end
 end
